4b0485bf230cc3ee52d1120ed64876b3debca4fa
tpamplin <gartiman1@gmail.com>
2024-06-04 10:03:57 -0400

    establish pokemonList Array

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: 70,
+        types: ["Grass", "Poison"]
+    }, 
+
+    {
+        name: "Charmander",
+        height: 60,
+        types: ["Fire"]
+    },
+
+    {
+        name: "Squirtle",
+        height: 50,
+        types: ["Water"]
+    }
+];
