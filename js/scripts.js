daa2a857b012d92bfd284bb97ca50a157f28914f
tpamplin <gartiman1@gmail.com>
2024-06-04 10:09:37 -0400

    added evolution pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,50 @@ let pokemonList = [
     }, 
 
     {
+        name: "Ivysaur",
+        height: 100,
+        types: ["Grass", "Poison"]
+    }, 
+
+    {
+        name: "Venusaur",
+        height: 200,
+        types: ["Grass", "Poison"]
+    }, 
+
+    {
         name: "Charmander",
         height: 60,
         types: ["Fire"]
     },
 
     {
+        name: "Charmeleon",
+        height: 110,
+        types: ["Fire"]
+    }, 
+
+    {
+        name: "Charizard",
+        height: 170,
+        types: ["Fire", "Flying"]
+    }, 
+
+    {
         name: "Squirtle",
         height: 50,
+        types: ["Water"]
+    },
+
+    {
+        name: "Wartortle",
+        height: 100,
+        types: ["Water"]
+    }, 
+
+    {
+        name: "Blastoise",
+        height: 160,
         types: ["Water"]
     }
 ];
