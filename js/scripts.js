0d34243c4ec5407c86a967aeab3c14b40ebb07a4
tpamplin <gartiman1@gmail.com>
2024-06-04 21:16:12 -0400

    improve formatting and readability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 //Pokedex Program!
 //Timothy Pamplin 2024
 
-//Pokemon data set.
+/* Pokemon Data Set
+
+Each pokemon has 3 properties:
+name
+height
+type
+
+*/
 let pokemonList = [
     {
         name: "Bulbasaur",
@@ -61,29 +68,44 @@ let pokemonList = [
 //establish variable to hold the height of the tallest pokemon
 let tallestHeight = 0;
 
-//find height of tallest pokemon
-//checks every pokemon in pokemonList to see if it is the tallest in the list.
-//sets tallestHeight to the height of the current pokemon if it is the tallest so far.
+/* find height of tallest pokemon
+
+checks every pokemon in pokemonList to see if it is the tallest in the list.
+sets tallestHeight to the height of the current pokemon if it is the tallest so far.
+
+*/
 for (let i = 0; i < pokemonList.length; i++){
+    
+    //set variables for pokemon name and height to improve readability.
+    let thisPokemonHeight = pokemonList[i].height;
+    
     //check if current pokemon is taller than tallest so far.
-    if (pokemonList[i].height > tallestHeight){
+    if (thisPokemonHeight > tallestHeight){
+    
         //set tallest height variable to current pokemon height.
-        tallestHeight = pokemonList[i].height;
+        tallestHeight = thisPokemonHeight;
     }
 };
 
-/*
-write data to page
+/* write data to page
 
 cycles through each pokemon and writes its name and height to the page, 
 also indentifies any pokemon whose height is equal to the tallest height
 and writes special message accordingly
+
 */
 for (let i = 0; i < pokemonList.length; i++){
+
+    //set variables for pokemon name and height to improve readability.
+    let thisPokemonName = pokemonList[i].name;
+    let thisPokemonHeight = pokemonList[i].height;
+    
     //write the name and height of the current pokemon.
-    document.write(`${pokemonList[i].name} -- Height: ${pokemonList[i].height} cm<br>`);
+    document.write(`${thisPokemonName} -- Height: ${thisPokemonHeight} cm<br>`);
+    
     //check to see if the current pokemon's height is the same as the tallest pokemon's height.
-    if (pokemonList[i].height == tallestHeight){
+    if (thisPokemonHeight == tallestHeight){
+        
         //if this is the tallest pokemon, tell me about it!
         document.write('WOW! that\'s big!<br>');
     };
