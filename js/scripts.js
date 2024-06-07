eafbaee937fc93b5790e004680a7f65bc2519868
tpamplin <gartiman1@gmail.com>
2024-06-07 17:14:27 -0400

    create pokemonRepository iife

--- js/scripts.js
+++ js/scripts.js
@@ -10,61 +10,79 @@ type
 
 */
 
-let pokemonList = [
-    {
-        name: "Bulbasaur",
-        height: 70,
-        types: ["Grass", "Poison"]
-    }, 
+let pokemonRepository = (function(){
 
-    {
-        name: "Ivysaur",
-        height: 100,
-        types: ["Grass", "Poison"]
-    }, 
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 70,
+            types: ["Grass", "Poison"]
+        }, 
 
-    {
-        name: "Venusaur",
-        height: 200,
-        types: ["Grass", "Poison"]
-    }, 
+        {
+            name: "Ivysaur",
+            height: 100,
+            types: ["Grass", "Poison"]
+        }, 
 
-    {
-        name: "Charmander",
-        height: 60,
-        types: ["Fire"]
-    },
+        {
+            name: "Venusaur",
+            height: 200,
+            types: ["Grass", "Poison"]
+        }, 
 
-    {
-        name: "Charmeleon",
-        height: 110,
-        types: ["Fire"]
-    }, 
+        {
+            name: "Charmander",
+            height: 60,
+            types: ["Fire"]
+        },
 
-    {
-        name: "Charizard",
-        height: 170,
-        types: ["Fire", "Flying"]
-    }, 
+        {
+            name: "Charmeleon",
+            height: 110,
+            types: ["Fire"]
+        }, 
 
-    {
-        name: "Squirtle",
-        height: 50,
-        types: ["Water"]
-    },
+        {
+            name: "Charizard",
+            height: 170,
+            types: ["Fire", "Flying"]
+        }, 
 
-    {
-        name: "Wartortle",
-        height: 100,
-        types: ["Water"]
-    }, 
+        {
+            name: "Squirtle",
+            height: 50,
+            types: ["Water"]
+        },
 
-    {
-        name: "Blastoise",
-        height: 160,
-        types: ["Water"]
+        {
+            name: "Wartortle",
+            height: 100,
+            types: ["Water"]
+        }, 
+
+        {
+            name: "Blastoise",
+            height: 160,
+            types: ["Water"]
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    };
+
+    function getAll () {
+
+        return pokemonList;
     }
-];
+
+    return {
+
+        add: add,
+        getAll: getAll
+    };
+})();
 
 /* write data to page
 
@@ -73,6 +91,8 @@ also indentifies any pokemon whose height is equal to the tallest height
 and writes special message accordingly
 
 */
+
+
 for (let i = 0; i < pokemonList.length; i++){
 
     //set variables for pokemon name and height to improve readability.
