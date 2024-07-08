bb7b04dc1513597e373ec4f138b71c7ff6c53803
tpamplin <gartiman1@gmail.com>
2024-07-08 16:44:59 -0400

    load 151 pokemon instead of 150

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 let pokemonRepository = (function(){
 
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
 
 
